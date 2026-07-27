641e08a3a4b1d8322920c36df930457bf95a16be
YuKyeong-L <y12k19@naver.com>
2026-07-27 11:07:18 +0900

    update: 경로

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://yukyeong-l.github.io/react-study/"
+  base: "/react-study/"
 })
