8e3be32e6bd91e62de145f93f9d9199f3fd6b85c
YuKyeong-L <y12k19@naver.com>
2026-08-06 17:04:02 +0900

    feat: Hankki propject
    Hankki propject

---
+++ src/han-kki/Button.jsx
@@ -0,0 +1,11 @@
+function MainBtn() {
+    return (
+        <>
+            <div className="flex justify-center mt-[80px]">
+                <button type="button" className="w-3xs h-[90px] bg-[#C44545] rounded-full font-SSE text-4xl text-white cursor-pointer hover:outline-[1px] hover:outline-black">한 끼 뽑기</button>
+            </div>
+        </>
+    )
+}
+
+export default MainBtn;
--- src/han-kki/Home.jsx
+++ src/han-kki/Home.jsx
@@ -1,11 +1,29 @@
 import { useState, useEffect } from 'react'
+import MainBtn from './Button';
+import './Hankki.css'
 
 function Home() {
     return (
         <>
-            <div>
-                오늘의 한 끼
-            </div>
+            <header className="header"></header>
+            <main className="main bg-[#5B7E3C] leading-[1.3]">
+                <section className="sec-main container">
+                    <div className="inner py-[100px] px-[20px]">
+                        <h1 className="font-SSE text-center text-6xl text-[#1D1D1D]">오늘의 한끼</h1>
+                        <div className="w-full mt-[70px] border-2 border-[#A2CB8B] bg-[#fff] rounded-3xl">
+                            <ul className="">
+                                <li>샌드위치</li>
+                                <li>치킨</li>
+                                <li>햄버거</li>
+                                <li>비빔밥</li>
+                                <li>짜장면</li>
+                            </ul>
+                        </div>
+                        <MainBtn></MainBtn>
+                    </div>
+                </section>
+            </main>
+            <footer></footer>
         </>
     )
 }
