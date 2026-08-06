b24c92c8f0ca29a97c5769d25b388198b366d7ee
YuKyeong-L <y12k19@naver.com>
2026-08-06 14:17:49 +0900

    refactor: 프로젝트 폴더 분리
    todo-list / han-kki 프로젝트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,17 @@
-import { useState } from 'react'
-import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import './App.css'
-import './todo-list/Todo.scss'
-import TodoList from "./todo-list/Todo.jsx";
+import TodoHome from "./todo-list/Home";
+import HanKkiHome from "./han-kki/Home";
 
 function App() {
-
-  const [now, setToday] = useState(new Date()); //날짜의 초기값
-  const month = now.getMonth() + 1;
-  const date = now.getDate();
-  const day = now.getDay();
-  const dayText = ['Sun', 'Mon', 'Tues', 'Weds', 'Thur', 'Fri', 'Sat'];
-
-  useEffect(() => {
-    //날짜 정보
-    const tomorrow = new Date(now);
-
-    tomorrow.setDate(tomorrow.getDate() + 1); //날짜 변경
-    tomorrow.setHours(0, 0, 0, 0); // 00:00
-    const timeout = tomorrow - now;
-
-    const timer = setTimeout(() => {
-      //자정이 되면 useState 상태를 변경
-      setToday(new Date());
-    }, timeout);
-
-    //타이머 정리
-    return function () {
-      clearTimeout(timer);
-    }
-  }, [now])
-
-  const [todos, setTodos] = useState(() => {
-    const savedData = localStorage.getItem("todos");
-
-    //갹체 생성
-    return savedData ? JSON.parse(savedData) : [];
-  });
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
-  const doneCount = todos.filter(todo => todo.done).length;
-  const donePercent = Math.round(doneCount / todos.length * 100);
-
-  function handleAdd() {
-    //배열의 뒤에 추가
-    //배열이 업데이트 됨과 동시에 화면에 li가 렌더링 됨
-    setTodos(todo => [
-      ...todo,
-      {
-        id: Date.now(),
-        text: "",
-        done: false,
-      }
-    ]);
-  }
-
   return (
-    <>
-      <header className="header"></header>
-      <main className="main">
-        <section className="sec-todo">
-          <div className="top-area">
-            <div className="top-inner">
-              <button type="button" className="add-todo" onClick={handleAdd}></button>
-              <h1 className="title">To-Do List</h1>
-            </div>
-          </div>
-          <div className="todo-area">
-            <TodoList todos={todos} setTodos={setTodos} />
-          </div>
-          <div className="total-area">
-            <div className="goal">
-              <span className="num">
-                {doneCount}/{todos.length}
-              </span>
-              <span className="percent">
-                {Number.isNaN(donePercent) ? 0 : donePercent}%
-              </span>
-            </div>
-            <p className="date">
-              {month < 10 ? '0' + month : month}/{date < 10 ? '0' + date : date} {dayText[day]}
-            </p>
-          </div>
-        </section>
-      </main>
-      <footer className="footer"></footer>
-    </>
-  )
+    <BrowserRouter basename="/react-study">
+      <Routes>
+        <Route path="/" element={<TodoHome />} />
+        <Route path="/han-kki/*" element={<HanKkiHome />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ src/han-kki/Home.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+
+function Home() {
+    return (
+        <>
+            <div>
+                오늘의 한 끼
+            </div>
+        </>
+    )
+}
+
+export default Home
